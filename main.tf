d7b6acdce8675a4db3b4d9dc0d70c677811940fa
JakeCP <jakecproctor@gmail.com>
2023-01-29 12:02:17 -0500

    fix remove target

--- main.tf
+++ main.tf
@@ -65,6 +65,15 @@ module "blog_alb" {
     bucket = "my-alb-logs"
   }
 
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
   http_tcp_listeners = [
     {
       port               = 80
